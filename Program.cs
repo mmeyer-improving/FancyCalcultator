a56a3a64cdbf31cf1d3c3dfba96ce534a7a0d325
Michael Meyer <michael.meyer@improving.com>
2021-07-12 08:23:18 -0500

    Second step, input validation, complete

--- Program.cs
+++ Program.cs
@@ -6,11 +6,24 @@ namespace FancyCalculator
     {
         static void Main(string[] args)
         {
+            decimal x;
+            decimal y;
+
             Console.WriteLine("A Console Calculator");
             Console.WriteLine("Enter a number.");
-            decimal x = Decimal.Parse(Console.ReadLine());
+            string firstInput = Console.ReadLine();
+            if(!(Decimal.TryParse(firstInput, out x)))
+            {
+                Console.WriteLine($"The first value, {firstInput}, is not a number.");
+                return;
+            }
             Console.WriteLine("Enter a second number, and I will add it to the first.");
-            decimal y = Decimal.Parse((Console.ReadLine()));
+            string secondInput = Console.ReadLine();
+            if(!(Decimal.TryParse(secondInput, out y)))
+            {
+                Console.WriteLine($"The second value, {secondInput}, is not a number.");
+                return;
+            }
             decimal result = x + y;
             Console.WriteLine($"Result: {result}");
         }
