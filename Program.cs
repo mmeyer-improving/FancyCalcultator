11ddec92ba41b864e5ff7bcfeff10e92bb7aa251
Michael Meyer <michael.meyer@improving.com>
2021-07-12 08:14:39 -0500

    Initial commit, step one completed

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FancyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A Console Calculator");
+            Console.WriteLine("Enter a number.");
+            decimal x = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a second number, and I will add it to the first.");
+            decimal y = Decimal.Parse((Console.ReadLine()));
+            decimal result = x + y;
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
